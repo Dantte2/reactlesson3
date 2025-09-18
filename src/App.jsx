cd5e1e84341728d75357f96523a92b7a4c4dcd46
Dantte2 <dino.sassi1@hotmail.fi>
2025-09-18 11:38:34 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Entry from "./components/Entry";
-import data from "./data"; 
+
 
 function App() {
   return (
-    <>
-      <Header />
-      {data.map(entry => (
-        <Entry
-          key={entry.id}
-          entry={entry}
-        />
-      ))}
-    </>
-  );
+    <p>hello</p>
+  )
 }
 
 export default App;
